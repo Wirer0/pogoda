dca02bf5480194bfc32bc59ae0eef2a8e6c31ab0
Wirer0 <ggrzybek246@gmail.com>
2026-03-24 21:53:37 +0100

    Update script.js

---
+++ script.js
@@ -0,0 +1,3 @@
+const apiKey = '1169bc38da3ae87b96edaf7b2027ef90'; 
+const city = 'Białystok';
+const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric&lang=pl`;
